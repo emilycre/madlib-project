280c596932c100f27989eafc49e8e50971886410
Emily Creamer <emily.creamer@icloud.com>
2019-01-17 21:05:42 -0800

    madlib words added

---
+++ main.js
@@ -0,0 +1,18 @@
+function madLib() {
+
+    var adjective = document.getElementById('adjective')
+    var place = document.getElementById('place')
+    var noun-1-insert = document.getElementById('noun-1-insert')
+    var noun-2-insert = document.getElementById('noun-2-insert')
+    var verb-insert = document.getElementById('verb-insert')
+    var action-verb-insert = document.getElementById('action-verb-insert')
+
+}
+
+// link each text input
+
+
+// retrieve the value of each text input
+
+
+// display the value of each text input
